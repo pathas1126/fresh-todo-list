8c0fe2c082842cec83ba235a5aa367360a1eba61
pathas <pathas@logiall.com>
2024-01-11 18:10:35 +0900

    data 파일 확장자 변경

--- routes/api/todos/repository.ts
+++ routes/api/todos/repository.ts
@@ -1,10 +1,10 @@
 import { Data, ITodo } from "../../../types/todo.types.ts";
 
 export const getData = async (): Promise<Data> =>
-  JSON.parse(await Deno.readTextFile("data.json")) as Data;
+  JSON.parse(await Deno.readTextFile("data.txt")) as Data;
 
 export const setData = async (todos: ITodo[]): Promise<void> =>
   await Deno.writeTextFile(
-    "data.json",
+    "data.txt",
     JSON.stringify({ todos }),
   );
