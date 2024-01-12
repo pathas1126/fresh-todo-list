507e4a841b40f0118b05d719b096e64ae3014f00
pathas <pathas@logiall.com>
2024-01-12 13:45:03 +0900

    요청 URL 확인 코드 추가

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,28 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { TodoList } from "../islands/TodoList.tsx";
 import { ITodo } from "../types/todo.types.ts";
+import { data } from "./api/todos/db.ts";
 
 interface HomeContext {
   todos: ITodo[];
+  test: string;
 }
 
 export const handler: Handlers<HomeContext> = {
   async GET(_req, ctx) {
+    let test = "";
     try {
+      test = `${ctx.url.origin}/api/todos`;
       const todos = await (await fetch(
         `${ctx.url.origin}/api/todos`,
         {
           method: "GET",
         },
       )).json();
-      return ctx.render({ todos });
+      return ctx.render({ todos, test });
     } catch (error) {
       console.log(error);
-      return ctx.render({ todos: [] });
+      return ctx.render({ todos: [], test });
     }
   },
 };
@@ -34,7 +38,7 @@ export default function Home(props: PageProps<HomeContext>) {
         alt="the Fresh logo: a sliced lemon dripping with juice"
       />
       <h1 class="text-4xl font-bold">Fresh Todo List</h1>
-
+      {props.data.test}
       <TodoList
         todos={props.data.todos}
       />
